82e159ce08eb8d19c35e5010a501f5489337be8c
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-02-23 06:06:17 +0530

    Update Q2.c

--- Day 5/Q2.c
+++ Day 5/Q2.c
@@ -1,9 +1,7 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    int l = m - 1;          // pointer for nums1's valid elements
-    int r = n - 1;          // pointer for nums2
-    int temp = m + n - 1;   // pointer for placement in nums1
-
-    // Merge from the back
+    int l = m - 1;          
+    int r = n - 1;          
+    int temp = m + n - 1;   
     while (l >= 0 && r >= 0) {
         if (nums1[l] > nums2[r]) {
             nums1[temp] = nums1[l];
@@ -14,8 +12,6 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
         }
         temp--;
     }
-
-    // Copy remaining nums2 elements (if any)
     while (r >= 0) {
         nums1[temp] = nums2[r];
         r--;
