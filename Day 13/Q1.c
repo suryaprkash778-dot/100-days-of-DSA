04af156b0f756ce3adba4134c51f4d606ecc046a
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-03-18 17:09:03 +0530

    Create Q1.c

---
+++ Day 13/Q1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int matrix[r][c];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+
+    while (top <= bottom && left <= right) {
+        // Traverse Left → Right
+        for (int i = left; i <= right; i++) {
+            printf("%d ", matrix[top][i]);
+        }
+        top++;
+
+        // Traverse Top → Bottom
+        for (int i = top; i <= bottom; i++) {
+            printf("%d ", matrix[i][right]);
+        }
+        right--;
+
+        // Traverse Right → Left
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                printf("%d ", matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        // Traverse Bottom → Top
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf("%d ", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return 0;
+}
