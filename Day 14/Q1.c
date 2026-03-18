1aa31c0eb50469cf508b7897e4492ae725d07b3e
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-03-18 17:11:36 +0530

    Create Q1.c

---
+++ Day 14/Q1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int isIdentity = 1; // assume true
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j && matrix[i][j] != 1) {
+                isIdentity = 0;
+                break;
+            }
+            if (i != j && matrix[i][j] != 0) {
+                isIdentity = 0;
+                break;
+            }
+        }
+        if (!isIdentity) break;
+    }
+
+    if (isIdentity)
+        printf("Identity Matrix\n");
+    else
+        printf("Not an Identity Matrix\n");
+
+    return 0;
+}
