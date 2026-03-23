c0db210089e457583ce661ce1b7c8b41994322d7
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-03-24 05:26:27 +0530

    Create Q2.c

---
+++ Day 20/Q2.c
@@ -0,0 +1,49 @@
+// Comparison function for qsort
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    // Allocate space for results (worst case: numsSize^2 triplets)
+    int capacity = numsSize * numsSize;
+    int** result = (int**)malloc(capacity * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(capacity * sizeof(int));
+    *returnSize = 0;
+
+    for (int i = 0; i < numsSize - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+
+        int left = i + 1;
+        int right = numsSize - 1;
+
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if (sum == 0) {
+                int* triplet = (int*)malloc(3 * sizeof(int));
+                triplet[0] = nums[i];
+                triplet[1] = nums[left];
+                triplet[2] = nums[right];
+
+                result[*returnSize] = triplet;
+                (*returnColumnSizes)[*returnSize] = 3;
+                (*returnSize)++;
+
+                // Skip duplicates
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
