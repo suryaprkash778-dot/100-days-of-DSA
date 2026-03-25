08d4e592e75b12ef925ee2e02fb2f193e0ac5aa4
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-03-25 05:39:47 +0530

    Implement function to reverse a linked list

---
+++ Day 22/Q2.c
@@ -0,0 +1,14 @@
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    struct ListNode* next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;   // store next node
+        curr->next = prev;   // reverse pointer
+        prev = curr;         // move prev forward
+        curr = next;         // move curr forward
+    }
+
+    return prev; // new head
+}
