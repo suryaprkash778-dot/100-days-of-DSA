ddf043b82fc57349ee01552bdc5d76ac93830209
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-03-28 10:19:52 +0530

    Create Q1.c

---
+++ Day 25/Q1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to count occurrences of key in linked list
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* current = head;
+    while (current != NULL) {
+        if (current->data == key) {
+            count++;
+        }
+        current = current->next;
+    }
+    return count;
+}
+
+int main() {
+    int n, key;
+    scanf("%d", &n);
+
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+
+    // Read n elements and build linked list
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Read key
+    scanf("%d", &key);
+
+    // Count occurrences
+    int result = countOccurrences(head, key);
+    printf("%d\n", result);
+
+    // Free memory
+    struct Node* current = head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    return 0;
+}
