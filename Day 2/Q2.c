9b436faff8e5df32c0141676c37d45f436f1accb
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-08-13 09:27:02 +0530

    Refactor maxProfit function for clarity and efficiency

--- Day 2/Q2.c
+++ Day 2/Q2.c
@@ -2,21 +2,19 @@
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0*/
 int maxProfit(int* prices, int pricesSize) {
-    int l = 0; // buy pointer
-    int r = 1; // sell pointer
-    int maxp = 0;
-
-    while (r < pricesSize) {
-        if (prices[r] > prices[l]) {
-            int currentp = prices[r] - prices[l];
-            if (currentp > maxp) {
-                maxp = currentp;
+    int min=0,max=1,maxp=0;
+    while(max<pricesSize){
+        if(prices[max]>prices[min]){
+            int currentp=prices[max]-prices[min];
+            if(currentp>maxp){
+                maxp=currentp;
             }
-        } else {
-            // found a cheaper buy price
-            l = r;
         }
-        r++;
+        else{
+            min=max;
+        }
+        max++;
     }
     return maxp;
 }
+  
