81c95105bbdb8f2eaea535581b140bb54659ec1f
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-08-19 07:18:38 +0530

    Update and rename Q2.c to move_zeroes.c

---
+++ Day 6/move_zeroes.c
@@ -0,0 +1,17 @@
+void moveZeroes(int* nums, int numsSize) {
+    int i=0,j=0;
+    while(i<numsSize){
+        if(nums[i]!=0){
+            nums[j]=nums[i];
+            j++;
+            i++;
+        }
+        else{
+            i++;
+        }
+    }
+    while(j<numsSize){
+        nums[j]=0;
+        j++;
+    }
+}
