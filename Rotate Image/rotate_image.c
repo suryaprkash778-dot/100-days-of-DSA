2df3015412221643181cf5e441d75c84f9bc5007
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-09-02 06:31:36 +0530

    Update and rename Q2.c to rotate_image.c

--- Rotate Image/rotate_image.c
+++ Rotate Image/rotate_image.c
@@ -1,7 +1,7 @@
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
     int n = matrixSize;
 
-    // Step 1: Transpose the matrix
+    // Step 1: Transpose
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             int temp = matrix[i][j];
@@ -14,9 +14,8 @@ void rotate(int** matrix, int matrixSize, int* matrixColSize) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n / 2; j++) {
             int temp = matrix[i][j];
-            matrix[i][j] = matrix[i][n - j - 1];
-            matrix[i][n - j - 1] = temp;
+            matrix[i][j] = matrix[i][n - 1 - j];
+            matrix[i][n - 1 - j] = temp;
         }
     }
 }
-
